0cc7fcf94aec60d9baf1fb45cb5f37787882a49e
leo <leonardo.arellano.97x@gmail.com>
2020-11-02 23:57:22 -0600

    move
    movimiento basico(muy)

---
+++ Characters/Player/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+export (int) var speed = 1;
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("Idle_Right");
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("M_right"):
+		$AnimationPlayer.play("Move_Right");
+		position.x+=speed;
+	else:
+		$AnimationPlayer.play("Idle_Right");
