1efb7214623b04389fa9ba7cec4d1889aeccb9a2
RodrigoSS <69469262+RoySS248@users.noreply.github.com>
2020-11-10 23:58:52 -0600

    Script Player

---
+++ Characters/Player/Script/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const SPEED : int = 45;
+const GRAVITY : int = 500;
+var velocity : Vector2 = Vector2.ZERO;
+var direction : Vector2 = Vector2.ZERO;
+var normalFloor : Vector2 = Vector2(0,-1);
+
+
+func _input(event):
+	direction.x = int(event.is_action_pressed("ui_right")) - int(event.is_action_pressed("ui_left"));
+	
+	if direction.x !=0:
+		$PositionPlayer.scale = Vector2(direction.x, 1);
+
+func _physics_process(delta):
+	apply_gravity(delta)
+
+ 
+func apply_gravity(delta):
+	velocity.y += GRAVITY * delta;
+	move_and_slide(velocity,normalFloor);
+	
+	if is_on_floor():
+		velocity.y = 0;
